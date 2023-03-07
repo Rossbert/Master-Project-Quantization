626cbf9fd98a75f0c8c39d23714328ff44ed3045
Rossbert <rosalesergius@hotmail.com>
2023-03-07 14:56:43 +0100

    Some updates in main code

--- First_test.py
+++ First_test.py
@@ -1,26 +1,42 @@
 import numpy as np
 import tensorflow as tf
 import tensorflow_model_optimization as tfmot
-# from datetime import datetime
+from datetime import datetime
 
 (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.fashion_mnist.load_data()
 
 # print(train_labels.shape)
 print(train_labels)
 
+
 model = tf.keras.Sequential([
     tf.keras.layers.Conv2D(32, 5, input_shape = (28, 28, 1)),
     tf.keras.layers.ReLU(),
+    tf.keras.layers.MaxPool2D(2),
+    tf.keras.layers.Conv2D(32, 5),
+    tf.keras.layers.ReLU(),
     tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(20, activation = 'relu'),
     tf.keras.layers.Dense(10, activation = 'softmax')
 ])
 
-# log_directory = "logs/fit/" + datetime.now().strftime("%Y%m%d-%H%H%S")
+# log_path = "logs/" + datetime.now().strftime("%Y%m%d-%H%H%S")
+normal_model_path = "cp_model.ckpt"
+directory_path = "logs/" + normal_model_path
 # tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir = log_directory)
+cp_normal_callback = tf.keras.callbacks.ModelCheckpoint(directory_path, save_weights_only = True, verbose = 1)
 
-model.compile(optimizer = tf.keras.optimizers.Adam(learning_rate = 0.01), loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+model.compile(optimizer = 'adam', # tf.keras.optimizers.Adam(learning_rate = 0.01), 
+    loss = 'sparse_categorical_crossentropy', 
+    metrics = ['accuracy'])
 model.summary()
-model.fit(train_images, train_labels, epochs = 5)
+model.fit(train_images, train_labels, 
+    epochs = 5,
+    callbacks = [cp_normal_callback])
+
+
+quantized_model_path = "logs/" + "Quantized_Model_Training"
+cp_quantized_callback = tf.keras.callbacks.ModelCheckpoint(quantized_model_path, save_weights_only = True, verbose = 1)
 
 quantize_model = tfmot.quantization.keras.quantize_model
 q_aware_model = quantize_model(model)
@@ -30,7 +46,9 @@ q_aware_model.compile(optimizer = 'adam',
     metrics = ['accuracy'])
 
 q_aware_model.summary()
-q_aware_model.fit(train_images, train_labels, epochs = 5)
+q_aware_model.fit(train_images, train_labels, 
+    epochs = 5,
+    callbacks = [cp_quantized_callback])
 
 _, baseline_model_accuracy = model.evaluate(test_images, test_labels)
 
